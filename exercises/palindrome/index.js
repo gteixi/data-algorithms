84ce5dfbdaf00efaad0517c7cf8cd325f8cb0296
gteixi <gemmateixido25@gmail.com>
2022-07-05 17:55:35 +0200

    exercise palindrome done

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -1,3 +1,4 @@
+// *********************************************************+
 // --- Directions
 // Given a string, return true if the string is a palindrome
 // or false if it is not.  Palindromes are strings that
@@ -6,7 +7,35 @@
 // --- Examples:
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
+// *********************************************************+
 
-function palindrome(str) {}
+// FIRST SOLUTION
+// function palindrome(str) {
+//   debugger;
+//   if (str.split("").reduce((strBackwards, str) => str + strBackwards) === str) {
+//     return true;
+//   }
+//   return false;
+// }
+
+// FIRST SOLUTION W REFRACTOR
+// function palindrome(str) {
+//   return (
+//     str.split("").reduce((strBackwards, str) => str + strBackwards) === str
+//   );
+// }
+
+// SECOND SOLUTION
+function palindrome(str) {
+  return str.split("").reverse().join("") === str;
+}
+
+// THIRD SOLUTION
+
+// function palindrome(str) {
+//   return str.split("").every((char, i) => {
+//     return char === str[str.length - i - 1];
+//   });
+// }
 
 module.exports = palindrome;
