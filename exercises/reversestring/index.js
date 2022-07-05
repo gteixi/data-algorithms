a98cb15151d33d740d8d6d8fe8d4be902ebd4b43
gteixi <gemmateixido25@gmail.com>
2022-07-05 17:55:14 +0200

    exercise reverse string done

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -1,3 +1,4 @@
+// *********************************************************+
 // --- Directions
 // Given a string, return a new string with the reversed
 // order of characters
@@ -5,7 +6,38 @@
 //   reverse('apple') === 'leppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
+// *********************************************************+
 
-function reverse(str) {}
+// FIRST SOLUTION WITH REVERSE()
+// function reverse(str) {
+//   const arr = str.split("");
+//   arr.reverse();
+//   return arr.join("");
+// }
+
+// FIRST SOLUTION W REFRACTOR
+// function reverse(str) {
+//   return str.split("").reverse().join("");
+// }
+
+// SECOND SOLUTION
+// function reverse(str) {
+//   let reverseStr = "";
+
+//   for (let char of str) {
+//     reverseStr = char + reverseStr  ;
+//   }
+//   return reverseStr;
+// }
+
+// THIRD SOLUTION
+function reverse(str) {
+  return str.split("").reduce((reverseStr, str) => {
+    debugger;
+    return str + reverseStr;
+  }, "");
+}
+
+reverse("hello");
 
 module.exports = reverse;
